085fe231d54caa7f20561f6531ec37634a6edafb
Nithin Kumar Kollerethu Suresh <nkollere@my.centennialcollege.ca>
2018-08-09 01:02:42 -0400

    first commit

---
+++ Scripts/objects/enemy.ts
@@ -0,0 +1,57 @@
+module objects {
+  export class Enemy extends objects.GameObject {
+    // private instance variables
+
+    // public properties
+
+    // Constructor
+    constructor() {
+      super("enemy");
+      this.Start();
+    }
+
+    // private methods
+
+    // public methods
+
+    // Initializes variables and creates new objects
+    public Start():void {
+     // this.x = 10;
+      //this.y = 10;
+      this._dy = 10;
+      this.Reset();
+    }
+
+    // updates the game object every frame
+    public Update():void {
+      this.Move();
+      this.CheckBounds();
+    }
+
+    // reset the objects location to some value
+    public Reset():void {
+      this.x = 80;
+      //Math.floor((Math.random() * (640 - this.width)) + this.halfWidth);
+      this.y = 80;
+      this.alpha = 0;
+    }
+
+    // move the object to some new location
+    public Move():void {
+      this.y += this._dy;
+    }
+
+    // check to see if some boundary has been passed
+    public CheckBounds():void {
+      // turn enemy back on when it appears on the screen
+      if((this.y >= 0) && (this.alpha == 0)) {
+        this.alpha = 1;
+      }
+
+      // check lower bounds
+      if(this.y >= 480 + this.height) {
+        this.Reset();
+      }
+    }
+  }
+}
